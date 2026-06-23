9b918de99d5d143d9ad6666cf780d4a52d4dfa18
Sushanth Reddy <suri868877@gmail.com>
2026-06-23 11:45:43 +0530

    .in

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -124,7 +124,7 @@ export function Footer() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                       </svg>
                     </span>
-                    info@steadcore.com
+                    info@steadcore.in
                   </a>
                   <a
                     href="tel:+917233022460"
